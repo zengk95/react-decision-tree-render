10482a7fc822bc461b8535821d79307fda82968a
Kerry Zeng <zengk95@gmail.com>
2019-03-21 10:43:21 -0500

    Added another component

---
+++ src/TestTree/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Table from "../Table";
+import './index.css';
+import * as data from '../resources.js';
+import Grid from '@material-ui/core/Grid'
+import MoviesAppBar from '../AppBar';
+
+class TestTree extends Component {
+  render() {
+    return <div>hello world</div>
+  }
+}
+
+export default TestTree;
